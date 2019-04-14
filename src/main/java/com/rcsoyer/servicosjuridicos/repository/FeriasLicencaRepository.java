e2402fadb9f5795d98a5b6a1fbe91c7a1932c818
rsoyer <rsoyer@carepayinternational.com>
2019-04-14 18:51:42 +0200

    bye bye birdy

--- src/main/java/com/rcsoyer/servicosjuridicos/repository/FeriasLicencaRepository.java
+++ src/main/java/com/rcsoyer/servicosjuridicos/repository/FeriasLicencaRepository.java
@@ -1,14 +1,13 @@
 package com.rcsoyer.servicosjuridicos.repository;
 
-import com.querydsl.core.types.Predicate;
+import static com.rcsoyer.servicosjuridicos.repository.FeriasLicencaRestrictions.getRestrictions;
+
 import com.rcsoyer.servicosjuridicos.domain.FeriasLicenca;
-import java.util.function.Function;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 import org.springframework.stereotype.Repository;
-
 
 /**
  * Spring Data JPA repository for the FeriasLicenca entity.
@@ -18,9 +17,6 @@ public interface FeriasLicencaRepository extends JpaRepository<FeriasLicenca, Lo
                                                  QuerydslPredicateExecutor<FeriasLicenca> {
     
     default Page<FeriasLicenca> query(final FeriasLicenca feriasLicenca, final Pageable pageable) {
-        Function<FeriasLicenca, Predicate> getPredicate = FeriasLicencaRestrictions::getRestrictions;
-        Function<Predicate, Page<FeriasLicenca>> findByPredicate = predicate -> findAll(predicate, pageable);
-        return getPredicate.andThen(findByPredicate)
-                           .apply(feriasLicenca);
+        return findAll(getRestrictions(feriasLicenca), pageable);
     }
 }
