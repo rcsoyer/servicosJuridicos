dc5373834c7c7e3919ac048864732b58c20d0da0
rsoyer <rsoyer@carepayinternational.com>
2019-04-14 20:23:07 +0200

    FeriasLicencaServiceImpl unit tests. making fields available for each test

--- src/test/java/com/rcsoyer/servicosjuridicos/service/impl/FeriasLicencaServiceImplTest.java
+++ src/test/java/com/rcsoyer/servicosjuridicos/service/impl/FeriasLicencaServiceImplTest.java
@@ -1,8 +1,10 @@
 package com.rcsoyer.servicosjuridicos.service.impl;
 
+import static java.util.Collections.emptyList;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
 import static org.mockito.Mockito.when;
 
 import com.rcsoyer.servicosjuridicos.domain.FeriasLicenca;
@@ -14,6 +16,8 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 @ExtendWith(SpringExtension.class)
@@ -42,17 +46,22 @@ class FeriasLicencaServiceImplTest {
         when(mapper.toEntity(dto)).thenReturn(domain);
         domain.setId(1L);
         when(repository.save(domain)).thenReturn(domain);
-        when(mapper.toDto(domain)).thenReturn(any(FeriasLicencaDTO.class));
+        when(mapper.toDto(domain)).thenReturn(dto.setId(1L));
         service.save(dto);
         verify(mapper, times(1)).toEntity(any(FeriasLicencaDTO.class));
         verify(repository, times(1)).save(any(FeriasLicenca.class));
+        verify(mapper, times(1)).toDto(any(FeriasLicenca.class));
+        verifyNoMoreInteractions(mapper, repository);
     }
     
     @Test
     void findAll() {
-        var dto = new FeriasLicencaDTO().setId(1L);
-        var domain = new FeriasLicenca();
-        when(mapper.toEntity(dto)).thenReturn(domain);
+        var pageable = PageRequest.of(0, 10);
+        var feriasLicencaPage = new PageImpl<FeriasLicenca>(emptyList());
+        when(repository.findAll(pageable)).thenReturn(feriasLicencaPage);
+        when(mapper.toDto(any(FeriasLicenca.class))).thenReturn(dto);
+        service.findAll(pageable);
+        verify(repository, times(1)).findAll(pageable);
     }
     
     @Test
