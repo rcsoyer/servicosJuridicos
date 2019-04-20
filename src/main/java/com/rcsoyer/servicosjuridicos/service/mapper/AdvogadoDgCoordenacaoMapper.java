9b7e2d9a9cc6345502f0b083e6cbd90e2ab749c1
rsoyer <rsoyer@carepayinternational.com>
2019-04-20 19:24:01 +0200

    some refactoring

--- src/main/java/com/rcsoyer/servicosjuridicos/service/mapper/AdvogadoDgCoordenacaoMapper.java
+++ src/main/java/com/rcsoyer/servicosjuridicos/service/mapper/AdvogadoDgCoordenacaoMapper.java
@@ -1,30 +1,28 @@
 package com.rcsoyer.servicosjuridicos.service.mapper;
 
-import com.rcsoyer.servicosjuridicos.domain.*;
+import com.rcsoyer.servicosjuridicos.domain.AdvogadoDgCoordenacao;
 import com.rcsoyer.servicosjuridicos.service.dto.AdvogadoDgCoordenacaoDTO;
-
-import org.mapstruct.*;
+import java.util.Optional;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 /**
  * Mapper for the entity AdvogadoDgCoordenacao and its DTO AdvogadoDgCoordenacaoDTO.
  */
 @Mapper(componentModel = "spring", uses = {AdvogadoMapper.class, CoordenacaoJuridicaMapper.class})
 public interface AdvogadoDgCoordenacaoMapper extends EntityMapper<AdvogadoDgCoordenacaoDTO, AdvogadoDgCoordenacao> {
-
+    
     @Mapping(source = "advogado.id", target = "advogadoId")
     @Mapping(source = "coordenacao.id", target = "coordenacaoId")
     AdvogadoDgCoordenacaoDTO toDto(AdvogadoDgCoordenacao advogadoDgCoordenacao);
-
+    
     @Mapping(source = "advogadoId", target = "advogado")
     @Mapping(source = "coordenacaoId", target = "coordenacao")
     AdvogadoDgCoordenacao toEntity(AdvogadoDgCoordenacaoDTO advogadoDgCoordenacaoDTO);
-
+    
     default AdvogadoDgCoordenacao fromId(Long id) {
-        if (id == null) {
-            return null;
-        }
-        AdvogadoDgCoordenacao advogadoDgCoordenacao = new AdvogadoDgCoordenacao();
-        advogadoDgCoordenacao.setId(id);
-        return advogadoDgCoordenacao;
+        return Optional.ofNullable(id)
+                       .map(new AdvogadoDgCoordenacao()::setId)
+                       .orElse(null);
     }
 }
