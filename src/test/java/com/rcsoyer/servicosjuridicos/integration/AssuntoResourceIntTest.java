628be04b2deb7aa8dbbab1473161f3824857da0c
rsoyer <rsoyer@carepayinternational.com>
2019-05-12 12:20:05 +0200

    AssuntoResource integration tests

--- src/test/java/com/rcsoyer/servicosjuridicos/integration/AssuntoResourceIntTest.java
+++ src/test/java/com/rcsoyer/servicosjuridicos/integration/AssuntoResourceIntTest.java
@@ -41,7 +41,7 @@ class AssuntoResourceIntTest extends ApiConfigTest {
     
     @Test
     void createAssunto_ok() throws Exception {
-        var assunto = assuntoDto();
+        var assunto = assuntoDto1();
         
         mockMvc.perform(
             post("/api/assunto")
@@ -56,7 +56,7 @@ class AssuntoResourceIntTest extends ApiConfigTest {
     
     @Test
     void createAssunto_alreadyHaveID() throws Exception {
-        var assunto = assuntoDto().setId(666L);
+        var assunto = assuntoDto1().setId(666L);
         
         mockMvc.perform(
             post("/api/assunto")
@@ -67,7 +67,7 @@ class AssuntoResourceIntTest extends ApiConfigTest {
     
     @Test
     void updateAssunto_ok() throws Exception {
-        var assunto = assuntoService.save(assuntoDto());
+        var assunto = assuntoService.save(assuntoDto1());
         
         mockMvc.perform(
             put("/api/assunto")
@@ -82,7 +82,7 @@ class AssuntoResourceIntTest extends ApiConfigTest {
     
     @Test
     void updateAssunto_dontHaveID() throws Exception {
-        var assunto = assuntoDto();
+        var assunto = assuntoDto1();
         
         mockMvc.perform(
             put("/api/assunto")
@@ -92,23 +92,52 @@ class AssuntoResourceIntTest extends ApiConfigTest {
     }
     
     @Test
-    void getAssuntos() throws Exception {
-        var assunto = assuntoService.save(assuntoDto());
+    void getAssuntos_matchingParams() throws Exception {
+        var assunto1 = assuntoService.save(assuntoDto1());
+        assuntoService.save(assuntoDto2());
+        
+        mockMvc.perform(
+            get("/api/assunto")
+                .param("peso", assunto1.getPeso().toString())
+                .param("ativo", assunto1.isAtivo().toString())
+                .param("descricao", assunto1.getDescricao()))
+               .andExpect(status().isOk())
+               .andExpect(jsonPath("$", hasSize(1)))
+               .andExpect(jsonPath("$.[0].id", equalTo(assunto1.getId().intValue())))
+               .andExpect(jsonPath("$.[0].ativo", equalTo(assunto1.isAtivo())))
+               .andExpect(jsonPath("$.[0].descricao", equalTo(assunto1.getDescricao())))
+               .andExpect(jsonPath("$.[0].peso", equalTo(assunto1.getPeso())));
+    }
+    
+    @Test
+    void getAssuntos_noParams() throws Exception {
+        var assunto1 = assuntoService.save(assuntoDto1());
+        var assunto2 = assuntoService.save(assuntoDto2());
         
         mockMvc.perform(
             get("/api/assunto"))
                .andExpect(status().isOk())
-               .andExpect(jsonPath("$", hasSize(1)))
-               .andExpect(jsonPath("$.[0].id", equalTo(assunto.getId().intValue())))
-               .andExpect(jsonPath("$.[0].ativo", equalTo(assunto.isAtivo())))
-               .andExpect(jsonPath("$.[0].descricao", equalTo(assunto.getDescricao())))
-               .andExpect(jsonPath("$.[0].peso", equalTo(assunto.getPeso())));
+               .andExpect(jsonPath("$", hasSize(2)))
+               .andExpect(jsonPath("$.[0].id", equalTo(assunto1.getId().intValue())))
+               .andExpect(jsonPath("$.[0].ativo", equalTo(assunto1.isAtivo())))
+               .andExpect(jsonPath("$.[0].descricao", equalTo(assunto1.getDescricao())))
+               .andExpect(jsonPath("$.[0].peso", equalTo(assunto1.getPeso())))
+               .andExpect(jsonPath("$.[1].id", equalTo(assunto2.getId().intValue())))
+               .andExpect(jsonPath("$.[1].ativo", equalTo(assunto2.isAtivo())))
+               .andExpect(jsonPath("$.[1].descricao", equalTo(assunto2.getDescricao())))
+               .andExpect(jsonPath("$.[1].peso", equalTo(assunto2.getPeso())));
     }
     
-    private AssuntoDTO assuntoDto() {
+    private AssuntoDTO assuntoDto1() {
         return new AssuntoDTO().setPeso(1)
                                .setAtivo(Boolean.TRUE)
                                .setDescricao("assuntoDto 1");
+    }
+    
+    private AssuntoDTO assuntoDto2() {
+        return assuntoService.save(new AssuntoDTO().setPeso(2)
+                                                   .setDescricao("assunto 2")
+                                                   .setAtivo(Boolean.FALSE));
     }
     
 }
