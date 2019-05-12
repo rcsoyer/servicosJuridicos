cba7a557ce9c2c0b807e37143c7cd0937f8ba9de
rsoyer <rsoyer@carepayinternational.com>
2019-05-12 03:52:30 +0200

    AssuntoResource integration tests

---
+++ src/test/java/com/rcsoyer/servicosjuridicos/integration/AppConfigTest.java
@@ -0,0 +1,25 @@
+package com.rcsoyer.servicosjuridicos.integration;
+
+import com.rcsoyer.servicosjuridicos.ServicosJuridicosApp;
+import com.rcsoyer.servicosjuridicos.web.rest.errors.ExceptionTranslator;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.web.PageableHandlerMethodArgumentResolver;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(classes = ServicosJuridicosApp.class)
+abstract class AppConfigTest {
+    
+    @Autowired
+    protected ExceptionTranslator exceptionTranslator;
+    
+    @Autowired
+    protected MappingJackson2HttpMessageConverter jacksonMessageConverter;
+    
+    @Autowired
+    protected PageableHandlerMethodArgumentResolver pageableArgumentResolver;
+    
+}
--- src/test/java/com/rcsoyer/servicosjuridicos/integration/AssuntoResourceIntTest.java
+++ src/test/java/com/rcsoyer/servicosjuridicos/integration/AssuntoResourceIntTest.java
@@ -1,28 +1,54 @@
 package com.rcsoyer.servicosjuridicos.integration;
 
-import com.rcsoyer.servicosjuridicos.ServicosJuridicosApp;
+import static org.hamcrest.Matchers.equalTo;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 import com.rcsoyer.servicosjuridicos.domain.Assunto;
 import com.rcsoyer.servicosjuridicos.repository.AssuntoRepository;
+import com.rcsoyer.servicosjuridicos.service.AssuntoService;
+import com.rcsoyer.servicosjuridicos.service.dto.AssuntoDTO;
+import com.rcsoyer.servicosjuridicos.web.rest.AssuntoResource;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
-@ExtendWith(SpringExtension.class)
-@SpringBootTest(classes = ServicosJuridicosApp.class)
-class AssuntoResourceIntTest {
+class AssuntoResourceIntTest extends AppConfigTest {
     
     @Autowired
     private AssuntoRepository assuntoRepository;
     
     @Autowired
-    private TestRestTemplate restTemplate;
+    private AssuntoService assuntoService;
+    
+    private MockMvc mockMvc;
+    
+    @BeforeEach
+    void setUp() {
+        var assuntoResource = new AssuntoResource(assuntoService);
+        this.mockMvc = MockMvcBuilders.standaloneSetup(assuntoResource)
+                                      .setCustomArgumentResolvers(pageableArgumentResolver)
+                                      .setControllerAdvice(exceptionTranslator)
+                                      .setMessageConverters(jacksonMessageConverter)
+                                      .build();
+    }
     
     @Test
-    void createAssunto() {
-        assuntoRepository.save(new Assunto().setPeso(1).setAtivo(Boolean.TRUE).setDescricao("assunto 1"));
+    void createAssunto() throws Exception {
+        var savedAssunto = assuntoRepository.save(new Assunto().setPeso(1)
+                                                               .setAtivo(Boolean.TRUE)
+                                                               .setDescricao("assunto 1"));
+        var dto = new AssuntoDTO().setPeso(1)
+                                  .setAtivo(Boolean.TRUE)
+                                  .setDescricao("assunto 1");
+        
+        mockMvc.perform(get("/api/assunto").param("peso", dto.getPeso().toString()))
+               .andExpect(status().isOk())
+               .andExpect(jsonPath("$.[0].id", equalTo(savedAssunto.getId().intValue())));
+        
     }
     
 }
