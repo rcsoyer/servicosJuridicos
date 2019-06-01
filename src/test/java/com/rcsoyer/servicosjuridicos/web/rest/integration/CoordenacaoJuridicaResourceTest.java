a5d90852c83d4f7c7cf2eb9048ffcdf6cee693be
rsoyer <rsoyer@carepayinternational.com>
2019-06-01 12:13:27 +0200

    CoordenacaoJuridicaResource integration tests

---
+++ src/test/java/com/rcsoyer/servicosjuridicos/web/rest/integration/CoordenacaoJuridicaResourceTest.java
@@ -0,0 +1,122 @@
+package com.rcsoyer.servicosjuridicos.web.rest.integration;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import com.rcsoyer.servicosjuridicos.service.AdvogadoService;
+import com.rcsoyer.servicosjuridicos.service.CoordenacaoJuridicaService;
+import com.rcsoyer.servicosjuridicos.service.dto.AssuntoDTO;
+import com.rcsoyer.servicosjuridicos.service.dto.CoordenacaoJuridicaDTO;
+import com.rcsoyer.servicosjuridicos.web.rest.AdvogadoResource;
+import com.rcsoyer.servicosjuridicos.web.rest.CoordenacaoJuridicaResource;
+import java.io.IOException;
+import java.util.Set;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.TestInstance.Lifecycle;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import static com.rcsoyer.servicosjuridicos.web.rest.TestUtil.convertObjectToJsonBytes;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.hasSize;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import com.rcsoyer.servicosjuridicos.service.AdvogadoService;
+import com.rcsoyer.servicosjuridicos.service.dto.AdvogadoDTO;
+import com.rcsoyer.servicosjuridicos.web.rest.AdvogadoResource;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.TestInstance.Lifecycle;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import static com.rcsoyer.servicosjuridicos.web.rest.TestUtil.convertObjectToJsonBytes;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.hasSize;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import com.rcsoyer.servicosjuridicos.service.AdvogadoService;
+import com.rcsoyer.servicosjuridicos.service.dto.AdvogadoDTO;
+import com.rcsoyer.servicosjuridicos.web.rest.AdvogadoResource;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.TestInstance.Lifecycle;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+@TestInstance(Lifecycle.PER_CLASS)
+class CoordenacaoJuridicaResourceTest extends ApiConfigTest {
+    
+    private static final String URL_COORDENACAO_API = "/api/coordenacao-juridica";
+    
+    private MockMvc mockMvc;
+    
+    @Autowired
+    private CoordenacaoJuridicaService coordenacaoService;
+    
+    @BeforeAll
+    void setUp() {
+        final var coordenacaoResource = new CoordenacaoJuridicaResource(coordenacaoService);
+        this.mockMvc = MockMvcBuilders.standaloneSetup(coordenacaoResource)
+                                      .setCustomArgumentResolvers(pageableArgumentResolver)
+                                      .setControllerAdvice(exceptionTranslator)
+                                      .setMessageConverters(jacksonMessageConverter)
+                                      .build();
+    }
+    
+    @Test
+    void createCoordenacaoJuridica() throws Exception {
+        final var dto = new CoordenacaoJuridicaDTO()
+                            .setSigla("ICLPOY")
+                            .setNome("Institute Cannabinistico Logistic OFF ya")
+                            .setCentena("421")
+                            .setAssuntos(Set.of(new AssuntoDTO().setId(1L),
+                                                new AssuntoDTO().setId(2L)));
+        
+        mockMvc.perform(post(URL_COORDENACAO_API)
+                            .contentType(MediaType.APPLICATION_JSON)
+                            .content(convertObjectToJsonBytes(dto)))
+               .andExpect(status().isCreated())
+               .andExpect(jsonPath("$.id").isNotEmpty())
+               .andExpect(jsonPath("$.id").isNumber())
+               .andExpect(jsonPath("$.nome").value(dto.getNome()))
+               .andExpect(jsonPath("$.sigla").value(dto.getSigla()))
+               .andExpect(jsonPath("$.centena").value(dto.getCentena()))
+               .andExpect(jsonPath("$.assuntos", hasSize(2)));
+    }
+    
+    @Test
+    void updateCoordenacaoJuridica() {
+    }
+    
+    @Test
+    void getCoordenacoes() {
+    }
+    
+    @Test
+    void getCoordenacaoJuridica() {
+    }
+    
+    @Test
+    void deleteCoordenacaoJuridica() {
+    }
+}
