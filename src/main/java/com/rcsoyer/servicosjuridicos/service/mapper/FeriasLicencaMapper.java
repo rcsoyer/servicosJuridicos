17e565ba35887fa933dffe39416e54e3e282adc2
rcsoyer <rcsoyer@gmail.com>
2019-06-09 19:01:06 +0200

    refactoring removing unnecessary method

--- src/main/java/com/rcsoyer/servicosjuridicos/service/mapper/FeriasLicencaMapper.java
+++ src/main/java/com/rcsoyer/servicosjuridicos/service/mapper/FeriasLicencaMapper.java
@@ -1,28 +1,20 @@
 package com.rcsoyer.servicosjuridicos.service.mapper;
 
-import com.rcsoyer.servicosjuridicos.domain.*;
+import com.rcsoyer.servicosjuridicos.domain.FeriasLicenca;
 import com.rcsoyer.servicosjuridicos.service.dto.FeriasLicencaDTO;
-
-import org.mapstruct.*;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 /**
  * Mapper for the entity FeriasLicenca and its DTO FeriasLicencaDTO.
  */
 @Mapper(componentModel = "spring", uses = {AdvogadoMapper.class})
 public interface FeriasLicencaMapper extends EntityMapper<FeriasLicencaDTO, FeriasLicenca> {
-
+    
     @Mapping(source = "advogado.id", target = "advogadoId")
     FeriasLicencaDTO toDto(FeriasLicenca feriasLicenca);
-
+    
     @Mapping(source = "advogadoId", target = "advogado")
     FeriasLicenca toEntity(FeriasLicencaDTO feriasLicencaDTO);
-
-    default FeriasLicenca fromId(Long id) {
-        if (id == null) {
-            return null;
-        }
-        FeriasLicenca feriasLicenca = new FeriasLicenca();
-        feriasLicenca.setId(id);
-        return feriasLicenca;
-    }
+    
 }
