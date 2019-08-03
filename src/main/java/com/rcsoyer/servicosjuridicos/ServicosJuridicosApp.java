d6587f17f65fac25c11d9987003f930c5350bccc
rcsoyer <rcsoyer@gmail.com>
2019-08-03 04:50:51 +0200

    ModalidadeService unit tests done

--- src/main/java/com/rcsoyer/servicosjuridicos/ServicosJuridicosApp.java
+++ src/main/java/com/rcsoyer/servicosjuridicos/ServicosJuridicosApp.java
@@ -18,56 +18,19 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.liquibase.LiquibaseProperties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.core.env.Environment;
 
 @Slf4j
 @SpringBootApplication
-@EnableDiscoveryClient
 @EnableConfigurationProperties({LiquibaseProperties.class, ApplicationProperties.class})
 public class ServicosJuridicosApp {
     
     private final Environment env;
     
-    public ServicosJuridicosApp(Environment env) {
+    public ServicosJuridicosApp(final Environment env) {
         this.env = env;
     }
     
-    /**
-     * Initializes servicosJuridicos.
-     * <p>
-     * Spring profiles can be configured with a program argument --spring.profiles.active=your-active-profile
-     * <p>
-     * You can find more information on how profiles work with JHipster on <a href="https://www.jhipster.tech/profiles/">https://www.jhipster.tech/profiles/</a>.
-     */
-    @PostConstruct
-    public void initApplication() {
-        var activeProfiles = asList(env.getActiveProfiles());
-        Predicate<List<String>> hasSpringProfileDev =
-            profiles -> profiles.contains(SPRING_PROFILE_DEVELOPMENT);
-        Predicate<List<String>> hasSpringProfileProd =
-            profiles -> profiles.contains(SPRING_PROFILE_PRODUCTION);
-        Predicate<List<String>> hasSpringProfileCloud =
-            profiles -> profiles.contains(SPRING_PROFILE_CLOUD);
-        
-        Optional.of(activeProfiles)
-                .filter(hasSpringProfileDev.and(hasSpringProfileProd))
-                .ifPresent(profiles ->
-                               log.error("You have misconfigured your application! It should not run " +
-                                             "with both the 'dev' and 'prod' profiles at the same time."));
-        
-        Optional.of(activeProfiles)
-                .filter(hasSpringProfileDev.and(hasSpringProfileCloud))
-                .ifPresent(profiles ->
-                               log.error("You have misconfigured your application! It should not " +
-                                             "run with both the 'dev' and 'cloud' profiles at the same time."));
-    }
-    
-    /**
-     * Main method, used to run the application.
-     *
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         final SpringApplication app = new SpringApplication(ServicosJuridicosApp.class);
         DefaultProfileUtil.addDefaultProfile(app);
@@ -76,18 +39,18 @@ public class ServicosJuridicosApp {
     }
     
     private static void logApplicationStartup(final Environment env) {
-        var protocol = Optional.ofNullable(env.getProperty("server.ssl.key-store"))
-                               .map(keyStore -> "https")
-                               .orElse("http");
-        var contextPath = Optional.ofNullable(env.getProperty("server.servlet.context-path"))
-                                  .orElse("/");
-        var serverPort = env.getProperty("server.port");
-        var hostAddress = Try.of(InetAddress::getLocalHost)
-                             .map(InetAddress::getHostAddress)
-                             .getOrElse(() -> {
-                                 log.warn("The host name could not be determined, using `localhost` as fallback");
-                                 return "localhost";
-                             });
+        String protocol = Optional.ofNullable(env.getProperty("server.ssl.key-store"))
+                                  .map(keyStore -> "https")
+                                  .orElse("http");
+        String contextPath = Optional.ofNullable(env.getProperty("server.servlet.context-path"))
+                                     .orElse("/");
+        String serverPort = env.getProperty("server.port");
+        String hostAddress = Try.of(InetAddress::getLocalHost)
+                                .map(InetAddress::getHostAddress)
+                                .getOrElse(() -> {
+                                    log.warn("The host name could not be determined, using `localhost` as fallback");
+                                    return "localhost";
+                                });
         log.info("\n----------------------------------------------------------\n\t" +
                      "Application '{}' is running! Access URLs:\n\t" +
                      "Local: \t\t{}://localhost:{}{}\n\t" +
@@ -102,6 +65,24 @@ public class ServicosJuridicosApp {
                  serverPort,
                  contextPath,
                  env.getActiveProfiles());
+    }
+    
+    @PostConstruct
+    private void checkActiveProfiles() {
+        final List<String> activeProfiles = asList(env.getActiveProfiles());
+        Predicate<List<String>> hasProfileDev = profiles -> profiles.contains(SPRING_PROFILE_DEVELOPMENT);
+        Predicate<List<String>> hasProfileProd = profiles -> profiles.contains(SPRING_PROFILE_PRODUCTION);
+        Predicate<List<String>> hasProfileCloud = profiles -> profiles.contains(SPRING_PROFILE_CLOUD);
+        
+        Optional.of(activeProfiles)
+                .filter(hasProfileDev.and(hasProfileProd))
+                .ifPresent(profiles -> log.error("The application might be misconfigured! It should not run " +
+                                                     "with both the 'dev' and 'prod' profiles at the same time."));
+        
+        Optional.of(activeProfiles)
+                .filter(hasProfileDev.and(hasProfileCloud))
+                .ifPresent(profiles -> log.error("The application might be misconfigured! It should not " +
+                                                     "run with both the 'dev' and 'cloud' profiles at the same time."));
     }
 }
 
---
+++ src/test/java/com/rcsoyer/servicosjuridicos/web/rest/integration/ModalidadeResourceIntTest.java
@@ -0,0 +1,34 @@
+package com.rcsoyer.servicosjuridicos.web.rest.integration;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import com.rcsoyer.servicosjuridicos.web.rest.integration.ApiConfigTest;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class ModalidadeResourceIntTest extends ApiConfigTest {
+    
+    @BeforeEach
+    void setUp() {
+    }
+    
+    @Test
+    void createModalidade() {
+    }
+    
+    @Test
+    void updateModalidade() {
+    }
+    
+    @Test
+    void getModalidades() {
+    }
+    
+    @Test
+    void getModalidade() {
+    }
+    
+    @Test
+    void deleteModalidade() {
+    }
+}
